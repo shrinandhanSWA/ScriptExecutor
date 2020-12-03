b5fcc76f095a325f3d5693e5d7e179e84b304611
sp2419 <sp2419@ic.ac.uk>
2020-12-03 19:44:24 +0700

    REFACTOR creation of input and output panels

--- src/ScriptRunner.java
+++ src/ScriptRunner.java
@@ -21,24 +21,9 @@ public class ScriptRunner {
 
     Container container = frame.getContentPane();
 
-    JPanel inputPanel = new JPanel();
-    inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
-    JTextField inputText = new JTextField("Input: ");
-    inputText.setEditable(false);
-    inputPanel.add(inputText);
-    JTextArea scriptField = new JTextArea("");
-    scriptField.setPreferredSize(new Dimension(400, 500)); // watch the size mates
-    inputPanel.add(scriptField);
+    CreateInputPanel(container);
 
-    JPanel outputPanel = new JPanel();
-    outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
-    JTextField outputText = new JTextField("Output: ");
-    outputText.setEditable(false);
-    JTextArea scriptOutputField = new JTextArea("");
-    scriptOutputField.setPreferredSize(new Dimension(300, 500));
-    scriptOutputField.setEditable(false);
-    outputPanel.add(outputText);
-    outputPanel.add(scriptOutputField);
+    JTextArea scriptOutputField = createOutputPanel(container);
 
     JPanel scriptExecutionInfo = new JPanel();
     JTextArea executingStatus =
@@ -63,12 +48,36 @@ public class ScriptRunner {
     scriptExecutionInfo.add(lastRunExitStatus);
     scriptExecutionInfo.add(executor);
 
-    container.add(inputPanel, BorderLayout.LINE_START);
-    container.add(outputPanel, BorderLayout.LINE_END);
     container.add(scriptExecutionInfo, BorderLayout.CENTER);
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
+  }
+
+  private JTextArea createOutputPanel(Container container) {
+    JPanel outputPanel = new JPanel();
+    outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
+    JTextField outputText = new JTextField("Output: ");
+    outputText.setEditable(false);
+    JTextArea scriptOutputField = new JTextArea("");
+    scriptOutputField.setPreferredSize(new Dimension(300, 500));
+    scriptOutputField.setEditable(false);
+    outputPanel.add(outputText);
+    outputPanel.add(scriptOutputField);
+    container.add(outputPanel, BorderLayout.LINE_END);
+    return scriptOutputField;
+  }
+
+  private void CreateInputPanel(Container container) {
+    JPanel inputPanel = new JPanel();
+    inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+    JTextField inputText = new JTextField("Input: ");
+    inputText.setEditable(false);
+    inputPanel.add(inputText);
+    JTextArea scriptField = new JTextArea("");
+    scriptField.setPreferredSize(new Dimension(400, 500)); // watch the size mates
+    inputPanel.add(scriptField);
+    container.add(inputPanel, BorderLayout.LINE_START);
   }
 
   public static void main(String[] args) {
